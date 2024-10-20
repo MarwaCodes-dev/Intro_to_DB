bbb4a0e10b8dfd3c7a95f58ff026040a6d961ac7
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-10-20 12:03:04 -0700

    updated

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,7 +25,8 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE
 );
-CREATE TABLE IF NOT EXISTS Order_details (
+
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
